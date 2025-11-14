50128bc2785b6f4c93f530aecc9434b68b5f9882
Petar Popovic <petarpnp@gmail.com>
2025-11-14 13:25:10 +0100

    Refactor mode image

---
+++ src/components/common/mode-image.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useTheme } from "next-themes";
+import Image, { ImageProps } from "next/image";
+
+const ModeImage = ({ image, imageDark, alt, ...props }: { image: string, imageDark: string, alt: string } & Omit<ImageProps, "src" | "alt">) => {
+  const { theme } = useTheme();
+
+  if (theme === "dark") {
+    return (
+      <Image {...props} className={cn("w-auto", props.className)} src={imageDark} alt={alt} width={100} height={100} /> // TODO: fix hardcoded width and height
+    );
+  }
+
+  return (
+    <div>
+      <Image {...props} className={cn("w-auto", props.className)} src={image} alt={alt} width={100} height={100} /> {/* TODO: fix hardcoded width and height */}
+    </div>
+  );
+}
+
+export default ModeImage;
--- src/components/landing/trusted-by.tsx
+++ src/components/landing/trusted-by.tsx
@@ -1,13 +1,15 @@
+import ModeImage from "../common/mode-image";
+
 const TrustedBy = () => {
   return (
     <div className="max-w-6xl w-full mx-auto px-4 py-20">
       <h2 className="text-sm text-foreground/50 text-center mb-5">Trusted by leading organizations and communities</h2>
       <div className="flex items-center justify-center gap-10">
-        <img className="h-8" src="/logos/eth-belgrade.svg" alt="ETH Belgrade logo" />
-        <img className="h-9" src="/logos/eth-floripa.png" alt="ETH Floripa logo" />
-        <img className="h-12" src="/logos/superteam-blkn.png" alt="Superteam Balkan logo" />
-        <img className="h-6" src="/logos/farcaster.svg" alt="Farcaster logo" />
-        <img className="h-7" src="/logos/lifi.svg" alt="LI.FI logo" />
+        <ModeImage className="h-8" image="/logos/eth-belgrade-light.svg" imageDark="/logos/eth-belgrade.svg" alt="ETH Belgrade logo" />
+        <ModeImage className="h-9" image="/logos/eth-floripa-pink.png" imageDark="/logos/eth-floripa.png" alt="ETH Floripa logo" />
+        <ModeImage className="h-11" image="/logos/superteam-blkn-light.png" imageDark="/logos/superteam-blkn.png" alt="Superteam Balkan logo" />
+        <ModeImage className="h-6" image="/logos/farcaster.svg" imageDark="/logos/farcaster.svg" alt="Farcaster logo" />
+        <ModeImage className="h-7" image="/logos/lifi-light.svg" imageDark="/logos/lifi.svg" alt="LI.FI logo" />
       </div>
     </div>
   );
