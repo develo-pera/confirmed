7a85c8637a18777df2072f7a6f34b00eea9766d6
Petar Popovic <petarpnp@gmail.com>
2025-11-18 23:59:06 +0100

    Add header section of profile page

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,18 +1,71 @@
 "use client";
 
-import { usePrivy } from "@privy-io/react-auth";
 import { Button } from "@/components/ui/button";
+import { usePrivy } from "@privy-io/react-auth";
+import Link from "next/link";
 
 const ProfilePage = () => {
   const { user, logout } = usePrivy();
 
+  console.log("user", user);
+
   return (
     <div className="max-w-6xl w-full mx-auto px-4 py-20 mt-20">
-      <div className="mb-5">
-        {JSON.stringify(user, null, 2)}
-      </div>
+      <div>
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-5">
+            <div className="w-26 h-26 rounded-full bg-foreground/10" />
+            <div>
+              <h2 className="text-2xl font-bold">
+                Name
+              </h2>
+              <p className="text-foreground/50 text-sm">
+                @username
+              </p>
+              <p className="text-foreground/50 text-sm mt-5">
+                Bio goes here on and on, and some more text here and there.
+              </p>
+              <div className="mt-1 flex items-center gap-2">
+                <Link href="https://website.com" target="_blank" rel="noopener noreferrer" className="text-sm">
+                  website.com
+                </Link>
+                <span className="text-foreground/50 text-sm">•</span>
+                <Link href="https://x.com" target="_blank" rel="noopener noreferrer" className="text-sm">
+                  x
+                </Link>
+                <span className="text-foreground/50 text-sm">•</span>
+                <Link href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-sm">
+                  Linkedin
+                </Link>
+                <span className="text-foreground/50 text-sm">•</span>
+                <Link href="https://farcaster.com" target="_blank" rel="noopener noreferrer" className="text-sm">
+                  Farcaster
+                </Link>
+              </div>
+            </div>
+          </div>
 
-      <Button onClick={logout}>Logout</Button>
+          {/* TODO: conditional display of edit profile button */}
+          <div>
+            <Button variant="secondary">
+              Edit profile
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="mt-10">
+        <div>
+          <h2 className="text-2xl font-bold">
+            Events
+          </h2>
+          <p className="text-foreground/50 text-sm">
+            View all your events, past and present.
+          </p>
+        </div>
+      </div>
+      {/* <div className="mb-5">
+        <pre className="text-sm">{JSON.stringify(user, null, 2)}</pre>
+      </div> */}
     </div>
   );
 }
