6693999ac7a5be6f1118d30a12e831c603796899
Petar Popovic <petarpnp@gmail.com>
2025-11-18 15:41:37 +0100

    Update hero section opn landing

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -3,7 +3,7 @@ import { RainbowButton } from "@/components/ui/rainbow-button"
 const Hero = () => {
   return (
     <div className="max-w-6xl w-full mx-auto px-4 pt-50 pb-0">
-      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-center mb-3 text-balance max-w-4xl mx-auto">Host events smarter. Fill rooms, not spreadsheets.</h1>
+      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-center mb-5 text-balance max-w-4xl mx-auto">Host events smarter. Fill rooms, not spreadsheets.</h1>
       <p className="text-center text-foreground/50 max-w-2xl mx-auto text-balance">
         {/* RSVPs with skin in the game. Confirmed helps event organizers reduce no-shows by letting attendees stake to secure their spot. */}
         {/* With powerful tools like stake to secure your spot and advanced analytics, Confirmed will make success out of your every event. */}
